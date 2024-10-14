cfc101710cc569bc701abdac2a11a2780bf0a5ec
Conar365 <kvazar382@gmail.com>
2024-10-15 00:08:45 +0300

    c++

--- web_external/installer/s.m
+++ web_external/installer/s.m
@@ -1,1 +1,1 @@
-g++ -std=c++17 -o main1.exe main.cpp src/httplib.cc src/server.cpp src/config.cpp src/html.cpp -lws2_32 -lole32 -lversion -lshlwapi
+g++ -std=c++17 -o main1.exe main.cpp lib/httplib.cc src/server.cpp src/config.cpp src/html.cpp -lws2_32 -lole32 -lversion -lshlwapi
