0909a9b5c6414e7fbd9268c7f1b5d5ddfb3ea452
Conar365 <kvazar382@gmail.com>
2024-10-20 00:10:47 +0300

    php site

---
+++ web_external/site/compilete_dev.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("node-sass static/prefab/main.scss static/css/main.css")
--- web_external/site/index.php
+++ web_external/site/index.php
@@ -1,12 +1,61 @@
-
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/config.php'; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php echo $content; ?>
+    <link rel="stylesheet" href="<?php echo $domen_name; ?>/style/main">
 </head>
 <body>
-    eee
+    <div class="menu">
+        <div onclick="scrollToElement('dwdd32444')" class="efw42f">Про програму</div>
+        <div onclick="scrollToElement('dd2343gvef')" class="efw42f">Скріни</div>
+        <div onclick="scrollToElement('ewwfrg34')" class="efw42f">Завантажити</div>
+        <div onclick="scrollToElement('we33333xxx')" class="efw42f">Вихідний код</div>
+    </div>
+    <p id="dwdd32444"></p>
+    <br><br><br><br><br><br><br><br><br><br>
+    <div class="w3fwe">
+        <div class="dwqs322v">
+        <span class="dq3dcccc">Security Hub</span> — це потужна антивірусна програма, розроблена для забезпечення комплексного захисту комп'ютера та даних користувача від вірусів, шкідливого ПЗ і небезпечних інтернет-загроз. Вона поєднує кілька основних функцій, які сприяють максимальному рівню безпеки як для локальних файлів, так і під час використання мережі інтернет.
+        </div>
+        <div class="dqwwqdcccc">
+            <div class="cover1212 atvImg">
+                <div class="atvImg-layer" data-img="static/img/9.png"></div>
+            </div>
+        </div>
+    </div>
+    <div id="dd2343gvef" class="rrggef">
+        <br><br><br><br>
+        <div class="dqd2323"></div>
+        <br><br><br>
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+        <img class="dqwd23d3ff" src="static/img/1.jpg">
+    </div>
+    <div id="ewwfrg34" class="rrggef">
+        <br><br><br><br>
+        <div class="dqd2323"></div>
+        <br><br><br>
+        <div class="fwf32rggg atvImg">
+            <div class="atvImg-layer" data-img="static/img/9.png"></div>
+        </div>
+        <br><br><br><br>
+        <button class="d3232f4ff">Скачати офлайн інсталятор</button>
+        <button class="d3232f4ff">Скачати онлайн інсталятор</button>
+    </div>
+    <div id="we33333xxx" class="rrggef">
+        <br><br><br><br>
+        <div class="dqd2323"></div>
+        <br><br><br>
+        <a href="https://github.com/Kvazar-213452/security_hub">https://github.com/Kvazar-213452/security_hub</a>
+    </div>
+    <br><br><br><br>
+    <script src="<?php echo $domen_name; ?>/lib/js/jquery"></script>
+    <script src="<?php echo $domen_name; ?>/js/main"></script>
 </body>
 </html>
