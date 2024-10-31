4a56219b99ad061042fdf6de0896f22270a4aa9b
Conar365 <kvazar382@gmail.com>
2024-10-31 21:20:04 +0300

    data

--- web_external/server/index.js
+++ web_external/server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 20991;
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,6 +15,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Сервер працює на http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Сервер працює на http://0.0.0.0:${PORT}`);
 });
