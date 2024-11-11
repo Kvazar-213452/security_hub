750fc23ecad4a42fb9866273b2febc1f57f09b78
Conar365 <kvazar382@gmail.com>
2024-11-11 21:46:59 +0300

    lang to shr

--- web/static/js/function.js
+++ web/static/js/function.js
@@ -836,7 +836,7 @@ $(".toggle-btn").click(function() {
 });
 
 function cleanup() {
-    message_window('Очищення компютера');
+    message_window("Очищення комп'ютера");
 
     $.ajax({
         url: "/cleanup",
@@ -975,6 +975,29 @@ function lang_change_page(lang) {
         // cleaning
         $('#vc728i32000').html("Cleaning pc");
         $('#bvh9iuweddd').html("Unfortunately, the script does not clean the System32 folder. Sorry");
+
+        $('#bx66210doddddw').html("Cleaning information");
+        $('#bo9ho2_0dddd').html("Clearing DNS and ARP tables");
+        $('#d_odw2983fff').html("File system logs");
+        $('#b8cq82d3333').html("Deleting the history of open paths");
+        $('#vc972odwe9').html("Clearing the internet cache");
+        $('#b89bdq9822222').html("Deleting specific files");
+        $('#bc92idw092222d').html("Deleting temporary files, cache and logs");
+        $('#vwer43erfdfcff').html("Additional settings");
+        $('#bvpuwe033230_233').html("Remove Windows backup copies");
+        $('#v21qp9wd000222s').html('The "Remove Windows backup copies" setting deletes saved system backups that were created for restoring Windows in case of failure or data loss. Backups may include system images (system backups), restore points, as well as automatic file archives.');
+        $('.cbq98222fz233').html("Install");
+        $('#cbwq982fff45t5').html("Delete all Wi-Fi profiles");
+        $('#cbqw278d436666').html('The "Delete all Wi-Fi profiles" setting will remove saved wireless network profiles that your device has connected to.');
+        $('#bv89qwdasdwededddd').html("Delete remote desktop connection settings.");
+        $('#vb0928923ee3ddd').html('The "Delete remote desktop connection settings" setting deletes saved configurations and connection history for remote desktops.');
+        $('#bviubiwdd3333r').html("Restart the doskey command processor");
+        $('#bciu292ed45675663').html(`
+            The "Restart doskey command processor" setting refreshes the doskey command processor.
+            <br><br>
+            Restarting doskey clears the command history and resets all defined macros. This can be useful for resetting the command line environment to its default state.
+        `);
+        $('#bf0qwp32r4t5651222').html("Clean the computer");
     } else if (lang === "uk") {
         // wifi
         $('#lang_wefsdeeeeee').html("Вайфай менеджер");
@@ -1008,7 +1031,28 @@ function lang_change_page(lang) {
         // cleaning
         $('#vc728i32000').html("Очищення ПК");
         $('#bvh9iuweddd').html("На жаль скрипт не очищує папку System32. Вибачте");
-
+        $('#bx66210doddddw').html("Інформація очистки");
+        $('#bo9ho2_0dddd').html("Очищення DNS та ARP-таблиць");
+        $('#d_odw2983fff').html("Журнали файлової системи");
+        $('#b8cq82d3333').html("Видалення історії відкритих шляхів");
+        $('#vc972odwe9').html("Очищення інтернет-кешу");
+        $('#b89bdq9822222').html("Видалення специфічних файлів");
+        $('#bc92idw092222d').html("Видалення тимчасових файлів, кешу та логів");
+        $('#vwer43erfdfcff').html("Додаткові налаштування");
+        $('#bvpuwe033230_233').html("Видалити резервні копії Windows");
+        $('#v21qp9wd000222s').html('Налаштування "Видалити резервні копії Windows" видаляє збережені резервні копії системи, які створювалися для відновлення Windows у випадку збою або втрати даних. Резервні копії можуть включати зображення системи (системні копії), точки відновлення, а також автоматичні архіви файлів.');
+        $('.cbq98222fz233').html("Встановити");
+        $('#cbwq982fff45t5').html("Видалити всі профілі Wi-Fi");
+        $('#cbqw278d436666').html('Налаштування "Видалити всі профілі Wi-Fi" призведе до видалення збережених профілів бездротових мереж, до яких підключався ваш пристрій.');
+        $('#bv89qwdasdwededddd').html("Видалити налаштування підключення до віддаленого робочого столу.");
+        $('#vb0928923ee3ddd').html('Налаштування "Видалити налаштування підключення до віддаленого робочого столу" видаляє збережені конфігурації та історію підключень до віддалених робочих столів.');
+        $('#bviubiwdd3333r').html("Перезапустити командний процесор doskey");
+        $('#bciu292ed45675663').html(`
+        Налаштування "Перезапускає командний процесор doskey" оновлює роботу командного процесора doskey
+                            <br><br>
+                            Перезапуск doskey очищає історію команд та скидає всі визначені макроси. Це може бути корисним для скидання середовища командного рядка до початкового стану.
+        `);
+        $('#bf0qwp32r4t5651222').html("Очистити комп'ютер");
     }
 }
 
