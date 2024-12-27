72939af6318d427e2770f8a0fc26fca43243e535
Kvazar-213452 <kvazar382@gmail.com>
2024-12-27 23:51:28 +0200

    bat fix

--- app_back_end/library/cleanup_script.bat
+++ app_back_end/library/cleanup_script.bat
@@ -58,3 +58,45 @@ del /f /s /q "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\WebCache\*.log
 del /f /s /q "C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Explorer\ThumbCacheToDelete\*.tmp" /s /q
 del /f /s /q "C:\Users\%USERNAME%\AppData\Local\Microsoft\Terminal Server Client\Cache\*.bin" /s /q
 del /f /s /q "C:\ProgramData\Microsoft\Windows\WER\*.*"
+
+
+cls
+
+@echo off
+
+timeout /t 1 /nobreak > NUL
+openfiles > NUL 2>&1
+if %errorlevel%==0 (
+    echo Running..
+) else (
+    echo You must run me as an Administrator. Exiting..
+    echo.
+    echo Right-click on me and select ^'Run as Administrator^' and try again.
+    echo.
+    echo Press any key to exit..
+    pause > NUL
+    exit
+)
+
+echo.
+
+del /s /f /q %WinDir%\Temp\*.*
+del /s /f /q %WinDir%\Prefetch\*.*
+del /s /f /q %Temp%\*.*
+del /s /f /q %AppData%\Temp\*.*
+del /s /f /q %HomePath%\AppData\LocalLow\Temp\*.*
+
+rd /s /q %WinDir%\Temp
+rd /s /q %WinDir%\Prefetch
+rd /s /q %Temp%
+rd /s /q %AppData%\Temp
+rd /s /q %HomePath%\AppData\LocalLow\Temp
+
+md %WinDir%\Temp
+md %WinDir%\Prefetch
+md %Temp%
+md %AppData%\Temp
+md %HomePath%\AppData\LocalLow\Temp
+
+echo.
+exit
