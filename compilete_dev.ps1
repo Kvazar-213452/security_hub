4395246dfb64290e07c233e8e2f6ba83e619bbfa
Conar365 <kvazar382@gmail.com>
2024-12-31 13:29:35 +0300

    fix

---
+++ compilete_dev.ps1
@@ -0,0 +1,48 @@
+$let = Read-Host "Type"
+
+# ⣾⡇⣿⣿⡇⣾⣿⣿⣿⣿⣿⣿⣿⣿⣄⢻⣦⡀⠁⢸⡌⠻⣿⣿⣿⡽⣿⣿
+# ⡇⣿⠹⣿⡇⡟⠛⣉⠁⠉⠉⠻⡿⣿⣿⣿⣿⣿⣦⣄⡉⠂⠈⠙⢿⣿⣝⣿
+# ⠤⢿⡄⠹⣧⣷⣸⡇⠄⠄⠲⢰⣌⣾⣿⣿⣿⣿⣿⣿⣶⣤⣤⡀⠄⠈⠻⢮
+# ⠄⢸⣧⠄⢘⢻⣿⡇⢀⣀⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⢀
+# ⠄⠈⣿⡆⢸⣿⣿⣿⣬⣭⣴⣿⣿⣿⣿⣿⣿⣿⣯⠝⠛⠛⠙⢿⡿⠃⠄⢸
+# ⠄⠄⢿⣿⡀⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⡾⠁⢠⡇⢀
+# ⠄⠄⢸⣿⡇⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⣫⣻⡟⢀⠄⣿⣷⣾
+# ⠄⠄⢸⣿⡇⠄⠈⠙⠿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⡿⢠⠊⢀⡇⣿⣿
+# ⠒⠤⠄⣿⡇⢀⡲⠄⠄⠈⠙⠻⢿⣿⣿⠿⠿⠟⠛⠋⠁⣰⠇⠄⢸⣿⣿⣿
+
+if ($let -eq 0) {
+    git add -A
+    $name = Read-Host "Name"
+    git commit -m $name
+    git push
+} elseif ($let -eq 1) {
+    node-sass app_front_end/static/prefab/scss/main.scss app_front_end/static/css/main.css
+    node-sass app_front_end/static/prefab/scss/global.scss app_front_end/static/css/global.css
+} elseif ($let -eq 2) {
+    Push-Location
+    try {
+        cd app_back_end
+        go build
+        .\head.exe
+    } finally {
+        Pop-Location
+    }
+} elseif ($let -eq 3) {
+    Push-Location
+    try {
+        cd app_back_end
+        go build -ldflags="-H windowsgui"
+        .\head.exe
+    } finally {
+        Pop-Location
+    }
+} elseif ($let -eq 4) {
+    Push-Location
+    try {
+        cd app_back_end
+        rsrc -ico icon.ico -o icon.syso
+        .\head.exe
+    } finally {
+        Pop-Location
+    }
+}
